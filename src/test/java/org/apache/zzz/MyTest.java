f63e0a8473f9c87169939227821ec2004ab23311
zhangsitian_bxbank <zhangsitian@aibank.com>
2020-03-05 18:22:30 +0800

    note

--- src/test/java/org/apache/zzz/MyTest.java
+++ src/test/java/org/apache/zzz/MyTest.java
@@ -2,10 +2,10 @@ package org.apache.zzz;
 
 import com.alibaba.fastjson.JSONObject;
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.AutoMappingBehavior;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,44 +18,27 @@ public class MyTest {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MyTest.class);
 
-    private SqlSessionFactory sqlSessionFactory;
+    private static final  String resource = "mybatis-hello.xml";
 
-    @Before
-    public void before()  throws IOException {
-        // mybatis配置文件
-        String resource = "mybatis-hello.xml";
-        // 得到配置文件流
-        InputStream inputStream =  Resources.getResourceAsStream(resource);
-        //创建会话工厂，传入mybatis配置文件的信息
-        sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-    }
-
-    @Test
-    public void findProductByIdTest() {
-        // 通过工厂得到SqlSession
-        SqlSession sqlSession = sqlSessionFactory.openSession();
-
-        // 通过SqlSession操作数据库
-        // 第一个参数：映射文件中statement的id，等于=namespace+"."+statement的id
-        // 第二个参数：指定和映射文件中所匹配的parameterType类型的参数
-        // sqlSession.selectOne结果 是与映射文件中所匹配的resultType类型的对象
-        // selectOne查询出一条记录
-        Product product = sqlSession.selectOne("test.findProductById", 1L);
-        LOGGER.info(JSONObject.toJSONString(product));
-        // 释放资源
-        sqlSession.close();
-
-    }
 
     // 根据用户名称模糊查询用户列表
     @Test
-    public void findProductByNameTest() {
+    public void findProductByNameTest() throws IOException {
+        // 得到配置文件流
+        InputStream inputStream =  Resources.getResourceAsStream(resource);
+        //创建会话工厂，传入mybatis配置文件的信息
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         // 通过工厂得到SqlSession
         SqlSession sqlSession = sqlSessionFactory.openSession();
         // list中的user和映射文件中resultType所指定的类型一致
         List<Product> list = sqlSession.selectList("test.findProductByName", "burger");
         sqlSession.close();
         LOGGER.info(JSONObject.toJSONString(list));
+    }
+
+    @Test
+    public void test(){
+        System.out.println(AutoMappingBehavior.NONE);
     }
 
 
