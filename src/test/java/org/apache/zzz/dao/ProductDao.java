d0f1da0693e78608946ae20484209750d89cf4c5
zhangsitian_bxbank <zhangsitian@aibank.com>
2020-05-14 13:01:51 +0800

    zzz

--- src/test/java/org/apache/zzz/dao/ProductDao.java
+++ src/test/java/org/apache/zzz/dao/ProductDao.java
@@ -5,5 +5,7 @@ import org.apache.zzz.Product;
 public interface ProductDao {
     Product findProductById(long id);
 
+    Product findProductByName(String name);
+
     int insertProduct(Product product);
 }
